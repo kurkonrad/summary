b4c147e56db6e64152113b2a1326b98dfe057d35
Konrad Kur <konradkur@gmail.com>
2017-09-02 23:23:36 +0200

    gulp, readme

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
+
+gulp.task('minify-css', function () {
+    gulp.src('assets/css/style.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('minify-js', function () {
+    gulp.src('assets/js/summary.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('build', ['minify-css', 'minify-js']);
