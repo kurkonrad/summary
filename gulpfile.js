f065102124cc4ef609ed8bb91b8c0a95f408f066
Konrad Kur <konradkur@gmail.com>
2017-09-02 23:56:55 +0200

    gulp task, gitignore, clean template

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ var gulp = require('gulp'),
     minifyCss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     connect = require('gulp-connect'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    rename = require('gulp-rename');
 
 gulp.task('minify-css', function () {
     gulp.src('assets/css/style.css')
+    .pipe(rename('style.min.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('minify-js', function () {
     gulp.src('assets/js/summary.js')
+    .pipe(rename('summary.min.css'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
 });
@@ -26,3 +29,4 @@ gulp.task('runserver', function() {
 });
 
 gulp.task('build', ['clean', 'minify-css', 'minify-js']);
+gulp.task('dev', ['clean', 'build', 'runserver']);
